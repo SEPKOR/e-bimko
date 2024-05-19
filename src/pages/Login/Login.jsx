a392e93fc509664c377276cc2fdd160227b2e5a7
SEPKOR <abizaralchifariputra@gmail.com>
2024-05-19 21:26:50 +0700

    Update Login.jsx

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -19,14 +19,14 @@ export default function login() {
                     <div className="card-body text-center">
                         <div className="items-center">
                             <label className="input input-bordered flex items-center gap-2">
-                                <link><svg
+                                <svg
                                     xmlns="http://www.w3.org/2000/svg"
                                     viewBox="0 0 16 16"
                                     fill="currentColor"
                                     className="w-4 h-4 opacity-70"
                                 >
                                     <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
-                                </svg></link>
+                                </svg>
                                 <input
                                     type="text"
                                     className="grow"
