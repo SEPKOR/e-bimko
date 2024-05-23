f71fc3c13d096727fa288166e106a5720545cc86
SEPKOR <abizaralchifariputra@gmail.com>
2024-05-23 20:29:57 +0700

    Update Verifikasi.jsx

--- src/pages/Login/Verifikasi.jsx
+++ src/pages/Login/Verifikasi.jsx
@@ -2,22 +2,6 @@ import { Link } from 'react-router-dom'
 export default function Verifikasi() {
     return (
         <body>
-            <div role="alert" className="alert">
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    className="stroke-info shrink-0 w-6 h-6"
-                >
-                    <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                    ></path>
-                </svg>
-                <span>Better Experien in Dark Mode</span>
-            </div>
             <img
                 width={250}
                 className="object-center h-auto m-auto z-auto flex justify-center content-center place-content-center"
