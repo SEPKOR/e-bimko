141af4a531199c97902d2aed95aae2c7c746c4b8
SEPKOR <abizaralchifariputra@gmail.com>
2024-05-23 20:37:50 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -26,8 +26,8 @@ import Developer from './pages/Developer.jsx'
 import TGB from './pages/Diagnostic/Tes2.jsx'
 import Ekstrakurikuler from './pages/Diagnostic/Tes1.jsx'
 import LoginAdmin from './pages/Login/LoginAdmin.jsx'
-import { SpeedInsights } from "@vercel/speed-insights/next"
 import { SpeedInsights } from '@vercel/speed-insights/react'
+
 const router = createBrowserRouter([
     {
         path: '/app',
